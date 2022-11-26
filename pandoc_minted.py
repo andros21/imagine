fdcb594e53b60f86578e7016995726ab2b989605
andros21 <andrea.ros21@e.email>
2022-11-26 14:34:54 +0100

    feat(pandoc_minted.py): support caption=|label= for `minted` blocks

--- pandoc_minted.py
+++ pandoc_minted.py
@@ -12,6 +12,7 @@ see: https://gist.github.com/jepio/3ecaa6bba2a53ff74f2e
 from pandocfilters import RawBlock, toJSONFilter
 
 TEMPLATE = r"""
+\begin{{listing}}[!ht]
 \begin{{minted}}[{options}]{{{lang}}}
 {cont}
 \end{{minted}}
@@ -29,7 +30,15 @@ def join_options(opts):
 def process_atts(kws):
     """Preprocess the attributes provided by pandoc - they come as a list of
     2-lists, convert to a list of strings"""
-    return ["%s=%s" % (l, r) for l, r in kws]
+    atts = {"caption": None, "label": None, "minted": []}
+    for ll, rr in kws:
+        if ll.lower() == "caption":
+            atts["caption"] = rr
+        elif ll.lower() == "label":
+            atts["label"] = rr
+        else:
+            atts["minted"].append("%s=%s" % (ll, rr))
+    return atts
 
 
 def main():
@@ -45,9 +54,16 @@ def main():
                     latex(
                         TEMPLATE.format(
                             lang=language,
-                            options=join_options(pos + atts),
+                            options=join_options(pos + atts["minted"]),
                             cont=contents,
                         )
+                        + (
+                            f"\n\\caption{{{atts['caption']}}}"
+                            if atts["caption"]
+                            else ""
+                        )
+                        + (f"\n\\label{{{atts['label']}}}" if atts["label"] else "")
+                        + ("\n\\end{listing}\n")
                     )
                 ]
 
